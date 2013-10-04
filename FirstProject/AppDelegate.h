3d45045f81dcb8b8a1bb2b5ceac54beb9cf3c4e8
James Alexander <jamesalexander@Jamess-MacBook-Pro.local>
2013-10-04 14:30:55 -0400

    Initial Commit

---
+++ FirstProject/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  FirstProject
+//
+//  Created by James Alexander on 10/4/13.
+//  Copyright (c) 2013 James Alexander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ FirstProject/MasterViewController.h
@@ -0,0 +1,22 @@
+//
+//  MasterViewController.h
+//  FirstProject
+//
+//  Created by James Alexander on 10/4/13.
+//  Copyright (c) 2013 James Alexander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+#import <CoreData/CoreData.h>
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+@end
